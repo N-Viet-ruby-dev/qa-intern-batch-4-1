297681e653472258924109b09825828af57e5116
hung ngo <ngotaihung@gmail.com>
2019-08-30 13:56:01 +0700

    Implement function Login User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger, :warning
+
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get "/home", to: "static_pages#home"
   get  "/register",  to: "users#new"
   post "/register",  to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
